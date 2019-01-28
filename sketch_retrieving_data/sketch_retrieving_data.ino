eb048723ef198c3c515d251e84d0b59f94110b77
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-01-28 16:05:55 +0100

    Added functionality with bluetooth

--- sketch_retrieving_data/sketch_retrieving_data.ino
+++ sketch_retrieving_data/sketch_retrieving_data.ino
@@ -1,5 +1,9 @@
-// Includes the Servo library
 #include <Servo.h>. 
+
+int ledblue=13;
+int tx=1;
+int rx=0;
+char inSerial[15];
 
 // Defines the Trigger and Echo pins of the Ultrasonic Sensor
 const int trigPin = 10;
@@ -16,35 +20,64 @@ long duration;
 int distance;
 Servo myServo; // Creates a servo object for controlling the servo motor
 
-void setup() 
-{
+
+void setup(){
+  Serial.begin(9600);
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600);
+  pinMode(ledblue, OUTPUT);
+  pinMode(tx, OUTPUT);
+  pinMode(rx, INPUT);
   myServo.attach(12); // Defines on which pin is the servo motor attached
 }
 
-void loop() 
-{
-  //Asking for resolution to user
- 
-  // rotates the servo motor from lowDegree to highDegree
-  for(int i=lowDegree;i<=highDegree;i++)
-  {  
-    myServo.write(i);
-    delay(30);
-    distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
-    printSerial(i); 
-    delay(30);
-  }
-  // Repeats the previous lines from highDegree to lowDegree
-  for(int i=highDegree;i>lowDegree;i--)
-  {  
-    myServo.write(i);
-    delay(30);
-    distance = calculateDistance();
-    printSerial(i);
-    delay(30);
+void loop(){
+  int i=0;
+  int m=0;
+  delay(500);
+  if (Serial.available() > 0) {
+    while (Serial.available() > 0) {
+      inSerial[i]=Serial.read();
+      i++;
+    }
+    inSerial[i]='\0';
+    if(!strcmp(inSerial,"2off")){ //Led Off
+      digitalWrite(ledblue, LOW);
+      for(m=0;m<11;m++){
+        inSerial[m]=0;
+      }
+      i=0;
+    }
+    if(!strcmp(inSerial,"2on")){ //Led on
+      digitalWrite(ledblue, HIGH);
+      for(int i=lowDegree;i<=highDegree;i++)
+      {  
+        myServo.write(i);
+        delay(30);
+        distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+        printSerial(i); 
+        delay(30);
+      }
+      // Repeats the previous lines from highDegree to lowDegree
+      for(int i=highDegree;i>lowDegree;i--)
+      {  
+        myServo.write(i);
+        delay(30);
+        distance = calculateDistance();
+        printSerial(i);
+        delay(30);
+      }
+      for(m=0;m<11;m++){
+        inSerial[m]=0;
+      }
+      i=0;
+    }
+    else{
+      for(m=0;m<11;m++){
+        inSerial[m]=0;
+      }
+      i=0;
+    }
   }
 }
 
